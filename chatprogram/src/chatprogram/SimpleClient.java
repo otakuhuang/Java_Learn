5a4ffd9a1e8281430c0a384d0ed5055a18bb2836
otakuhuang <shyxiaopang@gmail.com>
2020-10-14 23:32:23 +0800

    complete SimpleChat

--- chatprogram/src/chatprogram/SimpleClient.java
+++ chatprogram/src/chatprogram/SimpleClient.java
@@ -1,9 +1,11 @@
+package chatprogram;
+
 import java.io.IOException;
 import java.net.Socket;
 
 public class Client {
-    public static void main(String[] args) {
-
+    public static void main(String[] args) throws IOException {
+        commWithServer();
     }
 
     private static void commWithServer() throws IOException {
