b657e90299315ee2192007ce515c865ac37b8c96
otakuhuang <shyxiaopang@gmail.com>
2020-10-24 00:27:26 +0800

    complete chapter 9

---
+++ code/src/chapter_9/BigDecimalDemo.java
@@ -0,0 +1,20 @@
+package chapter_9;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
+public class Chapter_9_3 {
+
+    class BigDecimalDemo extends BigDecimal {
+
+        public BigDecimalDemo(long val) {
+            super(val);
+        }
+
+        
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
